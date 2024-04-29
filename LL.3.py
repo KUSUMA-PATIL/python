dbb86435de322263722207bef52fcaae93334be6
KUSUMA-PATIL <167950408+KUSUMA-PATIL@users.noreply.github.com>
2024-04-29 16:07:32 +0530

    Add files via upload

---
+++ LL.3.py
@@ -0,0 +1,13 @@
+
+class node:
+    def __init__(self.d):
+        self.data=d
+        self.next=None
+        
+class LL:
+    def __init__(self):
+        self.head=None
+        
+    def add_node(self.new_value):
+        new_node.next=self.head
+                         
